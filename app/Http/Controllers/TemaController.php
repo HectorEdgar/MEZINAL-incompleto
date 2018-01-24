2bbba3c63d8e94b9cad15c7322d40cf51f2cce3d
MRHE <hetorbite@gmail.com>
2018-01-24 13:29:50 -0600

    Se agregaron todos los archivos para la tabla tema

---
+++ app/Http/Controllers/TemaController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace practicasUnam\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TemaController extends Controller
+{
+    //
+}
---
+++ app/Tema.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace practicasUnam;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tema extends Model
+{
+    //
+}
