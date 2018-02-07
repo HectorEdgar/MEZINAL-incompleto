978fa8861510bb35eadce7fc5e9a65c72932e039
MRHE <hetorbite@gmail.com>
2018-02-06 18:19:17 -0600

    Se corrigio el diseño de la tabla y el titulo y boton de agregar

--- resources/views/autor/index.blade.php
+++ resources/views/autor/index.blade.php
@@ -5,48 +5,56 @@
 @section('contenido')
     <div class="row">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h3>Listado de Autores
-                <a href="autor/create"><button class="btn btn-success">Nuevo</button></a>
+            <h3 class="text-center">
+                Listado de Autores   
             </h3>
-            @include('autor.search')
+            <a href="autor/create"><button class="btn btn-success container-fluid">Nuevo</button></a> 
+            <div class="clearfix"><br></div><div class="clearfix"></div>
+            
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <div class="table-responsive">
-                <table class="table table-striped table-bordered table-condensed table-hover">
-                    <thead>
-                        <th>Id</th>
-                        <th>Pseudonimo</th>
-                        <th>Nombre</th>
-                        <th>Apellidos</th>
-                        <th>Opciones</th>
-                    </thead>
-                    @foreach($autores as $item)
+
+        <div class="table-responsive table-responsive-xl table-responsive-md table-responsive-lg table-responsive-sm">
+            @include('autor.search')
+            <table class="table table-hover table-sm ">
+                
+                <thead class="thead-dark">
                     <tr>
-                        <td>{{$item ->Id_autor}}</td>
-                        <td>{{$item ->pseudonimo}}</td>
-                        <td>{{$item ->nombre}}</td>
-                        <td>{{$item ->apellidos}}</td>
-                        <td>
-                            <a href="{{URL::action('AutorController@edit',$item ->Id_autor)}}">
-                                <button class="btn btn-info">
-                                    Editar
-                                </button>
-                            </a>
-                            <a href="" data-target="#modal-delete-{{$item->Id_autor}}" data-toggle="modal">
-                                <button class="btn btn-danger">
-                                    Eliminar
-                                </button>
-                            </a>
-                        </td>
+                        <th scope="col" class="text-center align-middle">Id</th>
+                        <th scope="col" class="text-center align-middle">Pseudonimo</th>
+                        <th scope="col" class="text-center align-middle">Nombre</th>
+                        <th scope="col" class="text-center align-middle">Apellidos</th>
+                        <th scope="col" class="text-center align-middle ">Opciones</th>
                     </tr>
-                    @include('autor.modal') @endforeach
-                </table>
-            </div>
+                </thead>
+                
+                <tbody>
+                    @foreach($autores as $item)
+                        <tr>
+                            <th scope="row"  class="text-center align-middle">{{$item ->Id_autor}}</td>
+                            <td  class="text-center align-middle">{{$item ->pseudonimo}}</td>
+                            <td class="text-center align-middle">{{$item ->nombre}}</td>
+                            <td class="text-center align-middle">{{$item ->apellidos}}</td>
+                            <td class="text-center align-middle ">
+                                <a href="{{URL::action('AutorController@edit',$item ->Id_autor)}}">
+                                    <button class="btn btn-info container-fluid btn-sm">
+                                        Editar
+                                    </button>
+                                </a>
+                                <a href="" data-target="#modal-delete-{{$item->Id_autor}}" data-toggle="modal">
+                                    <button class="btn btn-danger container-fluid btn-sm">
+                                        Eliminar
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                        @include('autor.modal') 
+                    @endforeach
+                </tbody>
+            </table>
             {{$autores->render()}}
             {{$numero}}
-        </div>
+        
     </div>
 
 @endsection
